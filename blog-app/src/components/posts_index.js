660663a189b3a823f4c8dcd85f4082f444c66f8d
Ryan Henry <ryan_yohannes@hotmail.com>
2018-02-02 01:40:16 +0700

    commit from pc

---
+++ blog-app/src/components/posts_index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions'
+
+class PostsIndex extends Component {
+
+    componentDidMount () {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return(
+            <div>
+                Posts Index
+            </div>
+        );
+    }
+}
+
+
+                         //Idetical with mapStateToProps
+export default connect(null, { fetchPosts }) (PostsIndex);
---
+++ blog-app/src/reducers/reducer_posts.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import { FETCH_POSTS } from '../actions';
+
+
+export default function(state = {}, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            console.log(action.payload.data); // [ post1, post2]
+
+         // convert array into object (using lodash)
+            return _.mapKeys(action.payload.data, 'id')
+        default:
+            return state;  
+    }
+}
